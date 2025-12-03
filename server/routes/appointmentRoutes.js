525ee925fabc9020672f9184101e5c47ef092def
raularpal <raularpalsanchez@gmail.com>
2025-12-03 23:11:21 +0100

    Delete server directory

--- server/routes/appointmentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getSlots, bookAppointment, cancelAppointment } = require('../controllers/appointmentController');
-
-router.get('/slots', getSlots);
-router.post('/book', bookAppointment);
-router.delete('/cancel/:id', cancelAppointment);
-
-module.exports = router;
